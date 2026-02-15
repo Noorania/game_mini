059fb1d55abe3fc289f10cfa6ef7360f61d3ff48
Noorania <1123150106@global.ac.id>
2026-02-15 16:52:42 +0700

    update flame game (background)

---
+++ lib/game/fruit_cathcer_game.dart
@@ -0,0 +1,14 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class FruitCatcherGame extends FlameGame {
+  @override
+  Color backgroundColor() => const Color.fromARGB(255, 244, 99, 205);
+
+  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -19,81 +19,64 @@ class MyApp extends StatelessWidget {
 }
 
 class GameScreen extends StatefulWidget {
-  const GameScreen({super.key});
  @override
  State<GameScreen> createState() => GameScreenState();}
 
 class GameScreenState extends State<GameScreen> {
-  final ValueNotifier<int> counter = ValueNotifier(1);
+  late FruitCatcherGame game; 
   @override
-  void initState() {
+  void initState() 
+  {
   super.initState();
+  game = FruitCatcherGame();
   }
- @override
-  Widget build(BuildContext context) {
+
+  final ValueNotifier<int> counter = ValueNotifier(0);
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
-      body: Column(
+      body : Stack(
         children: [
-          Expanded(
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 50,
-                  left: 20,
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.black54,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: ValueListenableBuilder<int>(
-                      valueListenable: counter,
-                      builder: (context, score, child) {
-                        return Text(
-                          'Score: $score',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
+          GameWidget(game: game),
+          Positioned(
+            top: 50,
+            left: 20,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                borderRadius: BorderRadius.circular(10),
+              ),
 
-                Positioned(
-                  top: 50,
-                  right: 20,
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.music_note, color: Colors.black),
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.volume_up, color: Colors.black),
-                        onPressed: () {},
-                      ),
-                    ],
+              child:  ValueListenableBuilder<int>(
+              valueListenable: counter,
+              builder: (context, score, child){
+                return Text(
+                  'Score: $score',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                ),
-              ],
-            ),
-          ),
-
-          // ✅ BUTTON DI LUAR STACK
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: ElevatedButton(
-              onPressed: () {
-                counter.value++;
+                );
               },
-              child: const Text("Tambah Score"),
-            ),
+              ),
+            )
           ),
+
+          //untuk gambar icon
+          Positioned(
+            top: 50,
+            right: 20,
+            child: Row(
+              children: [IconButton(icon: const Icon(Icons.music_note, color: Colors.black,),onPressed: ()   {},),
+              IconButton(icon: const Icon(Icons.volume_up, color: Colors.black,),onPressed: ()   {},),
+              ],
+            )
+            ),
         ],
-      ),
+
+      )
     );
   }
 
